b1a599fa35881a8d0b0b0a6ca0f274b1cfacc8d4
MrMax01 <massimo.mauro00@gmail.com>
2023-11-08 15:23:45 +0100

    first commit

---
+++ src/main/java/massimomauro/S6L3SpringWebandData/S6L3SpringWebAndDataApplication.java
@@ -0,0 +1,13 @@
+package massimomauro.S6L3SpringWebandData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S6L3SpringWebAndDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S6L3SpringWebAndDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/massimomauro/S6L3SpringWebandData/S6L3SpringWebAndDataApplicationTests.java
@@ -0,0 +1,13 @@
+package massimomauro.S6L3SpringWebandData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S6L3SpringWebAndDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
